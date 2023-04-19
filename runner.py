15e8446468cbf2eafefb45f917db545ffb3d38b7
Sam McElligott <sammcelligott@outlook.com>
2023-04-19 12:07:16 +0100

    update

--- runner.py
+++ runner.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 
 def error(msg):
@@ -11,5 +12,30 @@ tests = {
     "three-digit-number.py": [("7\n3\n8", "738"), ("4\n5\n1", "451"), ("9\n3\n1", "931")],
 }
 
+number_correct = 0
+
 for file, cases in tests.items():
     print(os.getcwd())
+    cwd = os.getcwd().lower()
+
+    if cwd != "c:\\users\\ctyblue\\desktop\\python\\exam":
+        error("CWD is not exam directory")
+
+    if os.path.exists("stdin.txt"):
+        subprocess.run(["del"])
+
+    with open("stdin.txt", "a") as f:
+        for test, _ in cases:
+            f.write(test)
+
+    output = ""
+    for _, answer in cases:
+        output += f"{answer}\n"
+
+    res = subprocess.run(["python", file, "<", "stdin.txt"])
+    if res.stdout == output:
+        number_correct += 1
+    else:
+        print("Test case failed")
+
+    print(number_correct)
